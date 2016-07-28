d77b2bb0e78a62ac540198819e7e50ed2bd16cf9
waldenn <poulalma@gmail.com>
2016-07-29 00:54:15 +0200

    positional sound, skysphere, more links

--- app/examples/bowling-vr/js/game.js
+++ app/examples/bowling-vr/js/game.js
@@ -21,6 +21,88 @@ var Game = function() {
 
 };
 
+Game.prototype.init = function() {
+
+	this.soundWood = new Howl( { src: [ 'res/sounds/wood.mp3' ] } );
+	this.soundBall = new Howl( { src: [ 'res/sounds/ball.mp3' ], loop: false } );
+
+	this.pinPositions = [];
+	this.score = 0;
+	this.upPins = [];
+	this.camSpeed = 0.15;
+
+	this.inputManager = new InputManager();
+	this.inputManager.init();
+
+	// setup three.js WebGL renderer. Note: Antialiasing is a big performance hit.
+	// only enable it if you actually need to.
+	this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+	this.renderer.setPixelRatio( window.devicePixelRatio );
+
+	// append the canvas element created by the renderer to document body element.
+	document.body.appendChild( this.renderer.domElement );
+
+	// create a three.js scene.
+	//var scene = new THREE.Scene();
+	this.scene = new Physijs.Scene;
+	this.scene.setGravity( new THREE.Vector3( 0, - 10, 0 ) );
+
+	// create a three.js camera.
+	this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
+
+	this.cameraObject = new THREE.Object3D();
+	this.cameraObject.add( this.camera );
+	this.scene.add( this.cameraObject );
+
+	// apply VR headset positional data to camera.
+	this.controls = new THREE.VRControls( this.camera );
+	this.controls.standing = true;
+
+	this.cameraObject.position.y = - 1;
+	this.cameraObject.position.z = 10;
+
+	// apply VR stereo rendering to renderer.
+	this.effect = new THREE.VREffect( this.renderer );
+	this.effect.setSize( window.innerWidth, window.innerHeight );
+
+	// ground physics
+	ground = new Physijs.BoxMesh(
+
+		new THREE.CubeGeometry( 10, 1, 100 ),
+
+		Physijs.createMaterial(
+			new THREE.MeshBasicMaterial( { color: 0x333333 } ),
+			this.groundFriction,
+			this.groundRestitution
+		),
+		0
+	);
+
+	ground.name = "ground";
+	ground.position.set( 0, -1.05, 0 );
+
+	//this.scene.add( ground );
+
+	this.scene.add( this.createSkysphere() );
+
+	// setup bowling lane
+	this.createLane();
+
+	// setup bowling pins
+	this.upPins = this.createBowlingPins( 0, -0.44, 0, 0.3048, 4 );
+
+	for ( var i = 0; i < this.upPins.length; i ++ ) {
+
+		this.scene.add( this.upPins[ i ] );
+
+	}
+
+	// add a repeating grid as a skybox. FIXME
+	var loader = new THREE.TextureLoader();
+	loader.load( 'img/box.png', this.onTextureLoaded.bind( this ) );
+
+}
+
 // Get the HMD, and if we're dealing with something that specifies
 // stageParameters, rearrange the scene.
 Game.prototype.setupStage = function() {
@@ -61,6 +143,32 @@ Game.prototype.setStageDimensions = function( stage ) {
 
 }
 
+Game.prototype.createSkysphere = function() {
+
+	// http://www.ianww.com/2014/02/17/making-a-skydome-in-three-dot-js/
+	var g = new THREE.SphereGeometry(3000, 60, 40);
+	var uniforms = {
+		  texture: { type: 't', value: THREE.ImageUtils.loadTexture('res/textures/clouds.jpg') }
+		  //texture: { type: 't', value: THREE.ImageUtils.loadTexture('res/textures/stars.jpg') }
+		  //texture: { type: 't', value: THREE.TextureLoader('res/textures/clouds.jpg') }
+		  // FIXME: http://stackoverflow.com/questions/35540880/three-textureloader-is-not-loading-images-files
+	};
+
+	var m = new THREE.ShaderMaterial( {
+		  uniforms:       uniforms,
+		  vertexShader:   document.getElementById('sky-vertex').textContent,
+		  fragmentShader: document.getElementById('sky-fragment').textContent
+	});
+
+	skySphere = new THREE.Mesh(g, m);
+	skySphere.scale.set(-1, 1, 1);
+	//skySpere.eulerOrder = 'XZY';
+	skySphere.renderOrder = -100.0;
+
+	return skySphere;
+}
+
+
 
 Game.prototype.createBowlingPins = function( firstPinPositionX, firstPinPositionY, firstPinPositionZ, spacing, rows ) {
 
@@ -72,11 +180,22 @@ Game.prototype.createBowlingPins = function( firstPinPositionX, firstPinPosition
 
 		for ( var nPins = 0; nPins < i; nPins ++ ) {
 
-			if ( even ) var offset = ( i / 2 * spacing ) - spacing / 2;
-			if ( ! even ) var offset = ( i / 2 - 0.5 ) * spacing;
+			var offset;
 
-			var base = new Physijs.BoxMesh( new THREE.BoxGeometry( 0.09, 0.32, 0.09 ), new THREE.MeshNormalMaterial() );
+			if ( even ){
+				offset = ( i / 2 * spacing ) - spacing / 2;
+			}
+			else {
+				offset = ( i / 2 - 0.5 ) * spacing;
+			}
+
+			var base = new Physijs.BoxMesh(
+				new THREE.BoxGeometry( 0.09, 0.32, 0.09 ),
+				new THREE.MeshNormalMaterial()
+			);
+
 			base.position.set( firstPinPositionX + ( offset - nPins * spacing ), firstPinPositionY + 0.15, firstPinPositionZ + ( i * - spacing ) );
+
 			base.name = 'pin';
 
 			/*var base = new Physijs.CylinderMesh( new THREE.CylinderGeometry( 0.057277, 0.03556, 0.085725, 8 ), Physijs.createMaterial(new THREE.MeshNormalMaterial(), 0, 1), 1.5 );
@@ -114,6 +233,7 @@ Game.prototype.createBowlingPins = function( firstPinPositionX, firstPinPosition
 		}
 
 	}
+
 	return pins;
 
 }
@@ -123,6 +243,7 @@ Game.prototype.createLane = function() {
 	var gutter2offset = 1.28;
 
 	var laneFriction = 100, laneRestitution = 0.1;
+
 	var lane = new Physijs.BoxMesh( new THREE.BoxGeometry( 1.0541, 0.1, 18.28800 ), Physijs.createMaterial( new THREE.MeshNormalMaterial(), laneFriction, laneRestitution ), 0 );
 	lane.position.y = - 0.5;
 	this.scene.add( lane );
@@ -176,78 +297,6 @@ Game.prototype.createLane = function() {
 }
 
 
-Game.prototype.init = function() {
-
-	this.soundWood = new Howl( { src: [ 'res/sounds/wood.mp3' ] } );
-
-	this.pinPositions = [];
-	this.score = 0;
-	this.upPins = [];
-	this.camSpeed = 0.05;
-
-	this.inputManager = new InputManager();
-	this.inputManager.init();
-	console.log( this.inputManager );
-
-	// setup three.js WebGL renderer. Note: Antialiasing is a big performance hit.
-	// only enable it if you actually need to.
-	this.renderer = new THREE.WebGLRenderer( { antialias: true } );
-	this.renderer.setPixelRatio( window.devicePixelRatio );
-
-	// append the canvas element created by the renderer to document body element.
-	document.body.appendChild( this.renderer.domElement );
-
-	// create a three.js scene.
-	//var scene = new THREE.Scene();
-	this.scene = new Physijs.Scene;
-	this.scene.setGravity( new THREE.Vector3( 0, - 10, 0 ) );
-
-	// create a three.js camera.
-	this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
-
-	this.cameraObject = new THREE.Object3D();
-	this.cameraObject.add( this.camera );
-	this.scene.add( this.cameraObject );
-
-	// apply VR headset positional data to camera.
-	this.controls = new THREE.VRControls( this.camera );
-	this.controls.standing = true;
-
-	this.cameraObject.position.y = - 1;
-	this.cameraObject.position.z = 10;
-
-	// apply VR stereo rendering to renderer.
-	this.effect = new THREE.VREffect( this.renderer );
-	this.effect.setSize( window.innerWidth, window.innerHeight );
-
-	// ground physics
-	/*ground = new Physijs.BoxMesh(
-	  new THREE.CubeGeometry( 50, 1, 600 ),
-	  Physijs.createMaterial( new THREE.MeshBasicMaterial( { color: 0x888888 } ),
-	  this.groundFriction, this.groundRestitution ),
-	  0
-	);
-
-	ground.name = "ground";
-	ground.position.set( 0, 0, 0 );
-	this.scene.add( ground );*/
-
-	// setup the bowling pins
-	this.createLane();
-	this.upPins = this.createBowlingPins( 0, - 0.207, 0, 0.3048, 4 );
-
-	for ( var i = 0; i < this.upPins.length; i ++ ) {
-
-		this.scene.add( this.upPins[ i ] );
-
-	}
-
-	// add a repeating grid as a skybox. FIXME
-	var loader = new THREE.TextureLoader();
-	loader.load( 'img/box.png', this.onTextureLoaded.bind( this ) );
-
-}
-
 Game.prototype.onTextureLoaded = function( texture ) {
 
 	texture.wrapS = THREE.RepeatWrapping;
@@ -281,7 +330,28 @@ Game.prototype.onTextureLoaded = function( texture ) {
 
 	ball.addEventListener( 'collision', function( other_object, linear_velocity, angular_velocity ) {
 
-		if ( other_object.name == 'pin' )this.soundWood.play();
+		if ( other_object.name == 'pin' ){
+
+			var id = this.soundWood.play();
+			
+			// Change the position and rate.
+			this.soundWood.pos(other_object.position.x, other_object.position.y, other_object.position.z, id);
+			this.soundWood.rate(0.7, id);
+			this.soundWood.volume(1, id);
+
+		}
+
+		if ( other_object.name == 'ground' ){
+
+			var id = this.soundBall.play();
+			
+			// Change the position and rate.
+			//this.soundBall.pos(other_object.position.x, other_object.position.y, other_object.position.z, id);
+			//this.soundWood.rate( 0.1, id);
+			this.soundBall.volume(1, id);
+
+		}
+
 
 	}.bind( this ) );
 
@@ -289,6 +359,7 @@ Game.prototype.onTextureLoaded = function( texture ) {
 	this.scene.add( ball );
 	ball.applyCentralImpulse( new THREE.Vector3( 0, 0, - 50 ) );
 
+	/*
 	arrow = new Physijs.PlaneMesh(
 		new THREE.BoxGeometry( 0.05, 0.01, 0.15 ),
 		new THREE.MeshBasicMaterial,
@@ -300,6 +371,7 @@ Game.prototype.onTextureLoaded = function( texture ) {
 	)
 	arrow.position.set( 0, - 0.3, 9 );
 	this.scene.add( arrow );
+	*/
 
 	// start the animation loop
 	requestAnimationFrame( this.animate.bind( this ) );
@@ -313,7 +385,7 @@ Game.prototype.onTextureLoaded = function( texture ) {
 
 Game.prototype.animate = function( timestamp ) {
 
-	document.getElementById( "hud" ).innerHTML = 'Score: ' + this.score;
+	document.getElementById( "hud" ).innerHTML = 'VR Bowling<br/>score: ' + this.score;
 
 	//console.log(this.upPins[3].position);
 
@@ -341,15 +413,12 @@ Game.prototype.animate = function( timestamp ) {
 	// render the scene through the manager.
 	this.manager.render( this.scene, this.camera, timestamp );
 
-	//console.log(this.inputManager.up);
-
 	requestAnimationFrame( this.animate.bind( this ) );
 
 }
 
 Game.prototype.onResize = function( e ) {
 
-	//FIXME
 	this.effect.setSize( window.innerWidth, window.innerHeight );
 	this.camera.aspect = window.innerWidth / window.innerHeight;
 	this.camera.updateProjectionMatrix();
@@ -358,47 +427,41 @@ Game.prototype.onResize = function( e ) {
 
 Game.prototype.onKeyDown = function( e ) {
 
-	console.log( 'key: ' + e.keyCode )
-	console.log( this.cameraObject.position );
+	//console.log( 'key: ' + e.keyCode )
+	//console.log( this.cameraObject.position );
 
-	if ( e.keyCode == 87 ) {
+	if ( e.keyCode == 87 ) { // w
 
-		//W
 		this.cameraObject.position.z -= this.camSpeed;
 
 	}
 
-	if ( e.keyCode == 83 ) {
+	if ( e.keyCode == 83 ) { // s
 
-		//S
 		this.cameraObject.position.z += this.camSpeed;
 
 	}
 
-	if ( e.keyCode == 65 ) {
+	if ( e.keyCode == 65 ) { // a
 
-		//A
 		this.cameraObject.position.x -= this.camSpeed;
 
 	}
 
-	if ( e.keyCode == 68 ) {
+	if ( e.keyCode == 68 ) { // d
 
-		//D
 		this.cameraObject.position.x += this.camSpeed;
 
 	}
 
-	if ( e.keyCode == 32 ) {
+	if ( e.keyCode == 32 ) { // space
 
-		//Space
 		this.cameraObject.position.y += this.camSpeed;
 
 	}
 
-	if ( e.keyCode == 17 ) {
+	if ( e.keyCode == 17 ) { // ctrl
 
-		//Ctrl
 		this.cameraObject.position.y -= this.camSpeed;
 
 	}
